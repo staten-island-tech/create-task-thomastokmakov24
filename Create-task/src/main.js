06d034b9e2a255ea482d39ee5d40658e66320778
thomastokmakov24 <thomast89@nycstudents.net>
2026-01-27 12:31:41 -0500

    gguguguguug
    g

--- Create-task/src/main.js
+++ Create-task/src/main.js
@@ -1,2 +1,13 @@
 import './style.css'
 
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+  }
+
+const gay = getRandomInt(1, 10);
+console.log(gay);
+
+function check(userInput) {
+    
